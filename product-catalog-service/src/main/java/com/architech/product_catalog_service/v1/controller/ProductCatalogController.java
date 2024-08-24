05661eaca3130920864aeb4d9c8ebdf2e2e9305b
archbong <archibong.chinonso.samuel@gmail.com>
2024-08-24 05:05:01 +0100

    added user and product-catalog-services

---
+++ product-catalog-service/src/main/java/com/architech/product_catalog_service/v1/controller/ProductCatalogController.java
@@ -0,0 +1,27 @@
+package main.java.com.architech.product_catalog_service.v1.controller;
+
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+@RestController
+public class ProductCatalogController {
+    
+    private final DiscoveryClient discoveryClient;
+    private final RestClient restClient;
+
+    public ProductCatalogController(DiscoveryClient discoveryClient, RestClient.Builder restClientBuilder) {
+        this.discoveryClient = discoveryClient;
+        restClient = restClientBuilder.build();
+    }
+
+    @GetMapping("helloEureka")
+    public String helloWorld() {
+        ServiceInstance serviceInstance = discoveryClient.getInstances("user").get(0);
+        return restClient.get()
+          .uri(serviceInstance.getUri() + "/helloWorld").retrieve().body(String.class);
+    }
+}
